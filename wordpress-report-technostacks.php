9dd9117eed85bd5a39252a37b46b21ddc5e25ce9
Darshan <darshan.patani@technostacks.com>
2023-08-16 12:30:18 +0530

    second commit

---
+++ wordpress-report-technostacks.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name:       WordPress Reports
+ * Plugin URI:        https://technostacks.com/
+ * Description:       Easily review and export Wordpress Reports, helps when you try to migrate website and need to know the status of each sites.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Darshan patani
+ * Author URI:        https://technostacks.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wordpress-report-technostacks
+ */
